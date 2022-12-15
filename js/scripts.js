23e3b19dac2154781b8b0e6414aa694a355ec507
Sean Vu <sonv98@gmail.com>
2022-12-15 17:51:47 -0600

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function() {  // Handler for .ready() called.
+    // Setting interval speed and toggle between pause/play based on click.
+    $(".carousel").carousel( { interval: 2000, pause: "false" } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
